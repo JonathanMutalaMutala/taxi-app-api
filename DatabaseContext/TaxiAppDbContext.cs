d614aad6c4051b63dac9c43dacbd9778a69b3b33
Mutala Mutala <jonamut12.live@gmail.com>
2024-06-27 21:45:08 -0400

    Ajout du dbContext.

---
+++ DatabaseContext/TaxiAppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using taxi_app_api.Models;
+
+namespace taxi_app_api.DatabaseContext
+{
+    public class TaxiAppDbContext : DbContext
+    {
+        private readonly DbContextOptions<TaxiAppDbContext> options;
+
+        public TaxiAppDbContext(DbContextOptions<TaxiAppDbContext> options) : base(options)
+        {
+            this.options = options;
+        }
+
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<CarOwner> CarOwners { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override  void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(TaxiAppDbContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
--- Extensions/ServiceRegistration.cs
+++ Extensions/ServiceRegistration.cs
@@ -6,8 +6,10 @@ namespace taxi_app_api.Extensions
 {
     public static class ServiceRegistration
     {
-        public static IServiceCollection AddRegistredServices(this IServiceCollection services)
+        public static IServiceCollection AddRegistredServices(this IServiceCollection services, IConfiguration configuration)
         {
+           // services.AddDbContext<>
+
             services.AddScoped<ICarOwner, CarOwnerService>(); 
             services.AddScoped<ICourseService, CourseService>();
             services.AddScoped<ICarService, CarService>();
