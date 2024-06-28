fcf63851f78ed50b5201556436d227ae40fc4d5f
Mutala Mutala <jonamut12.live@gmail.com>
2024-06-27 22:20:29 -0400

    Add ConnectionString

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddRegistredServices();
+builder.Services.AddRegistredServices(builder.Configuration);
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
