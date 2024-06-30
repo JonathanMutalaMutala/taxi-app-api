8ca368a2386aa2da1e3fcb14a709b2768976ce35
Mutala Mutala <jonamut12.live@gmail.com>
2024-06-30 00:19:43 -0400

    .

--- Utilities/ErrorDetails.cs
+++ Utilities/ErrorDetails.cs
@@ -7,9 +7,5 @@ namespace taxi_app_api.Utilities
         public int StatusCode { get; set; }
         public string Message { get; set; }
 
-        public override string ToString()
-        {
-            return JsonSerializer.Serialize(this);
-        }
     }
 }
